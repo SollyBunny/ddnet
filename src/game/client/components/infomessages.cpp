5776569d9694ee6933788202548451bb05f26f78
Alexander Akulich <akulichalexander@gmail.com>
2021-12-15 22:22:07 +0300

    CInfoMessages: Add a future-prove flag to skip NETMSGTYPE_SV_KILLMSGs

--- src/game/client/components/infomessages.cpp
+++ src/game/client/components/infomessages.cpp
@@ -245,6 +245,11 @@ void CInfoMessages::OnTeamKillMessage(const CNetMsg_Sv_KillMsgTeam *pMsg)
 
 void CInfoMessages::OnKillMessage(const CNetMsg_Sv_KillMsg *pMsg)
 {
+	const int InfClassModeSpecialSkip = 0x100;
+	if(pMsg->m_ModeSpecial == InfClassModeSpecialSkip)
+	{
+		return;
+	}
 	CInfoMsg Kill = CreateInfoMsg(TYPE_KILL);
 
 	if(m_pClient->m_aClients[pMsg->m_Victim].m_Active)
