d05753309839231cde563bc7fadc5b7026c415f3
Alexander Akulich <akulichalexander@gmail.com>
2022-05-22 18:25:21 +0300

    Prediction: Mark grenades as not explosive for certain classes

--- src/game/client/prediction/entities/character.cpp
+++ src/game/client/prediction/entities/character.cpp
@@ -479,6 +479,22 @@ void CCharacter::FireWeapon()
 	{
 		int Lifetime = (int)(GameWorld()->GameTickSpeed() * GetTuning(GetOverriddenTuneZone())->m_GrenadeLifetime);
 
+		bool Explosive = true;
+		if(GameWorld()->m_WorldConfig.m_IsInfClass)
+		{
+			switch(GetPlayerClass())
+			{
+			case PLAYERCLASS_MERCENARY:
+			case PLAYERCLASS_MEDIC:
+			case PLAYERCLASS_NINJA:
+			case PLAYERCLASS_SCIENTIST:
+				Explosive = false;
+				break;
+			default:
+				break;
+			}
+		}
+
 		new CProjectile(
 			GameWorld(),
 			WEAPON_GRENADE, //Type
@@ -487,7 +503,7 @@ void CCharacter::FireWeapon()
 			Direction, //Dir
 			Lifetime, //Span
 			false, //Freeze
-			true, //Explosive
+			Explosive, // Explosive
 			SOUND_GRENADE_EXPLODE //SoundImpact
 		); //SoundImpact
 	}
