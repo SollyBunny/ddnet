aacb5209953de3dfe74c9457f56949a431269e35
Alexander Akulich <akulichalexander@gmail.com>
2024-12-11 19:12:15 +0300

    Fixup the server startup with Infclass config file

--- src/engine/server/main.cpp
+++ src/engine/server/main.cpp
@@ -14,6 +14,7 @@
 
 #include <engine/shared/assertion_logger.h>
 #include <engine/shared/config.h>
+#include <engine/shared/infclass.h>
 
 #include <game/version.h>
 
@@ -156,6 +157,9 @@ int main(int argc, const char **argv)
 
 	IGameServer *pGameServer = CreateGameServer();
 	pKernel->RegisterInterface(pGameServer);
+	
+	const char aInfclassConfigFile[] = "settings_infclass.cfg";
+	SetInfclassConfigDomainId(pConfigManager->RegisterConfigDomain(aInfclassConfigFile));
 
 	pEngine->Init();
 	pConsole->Init();
