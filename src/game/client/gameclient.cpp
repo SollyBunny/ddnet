infclass/infclass-client

Add bio mine icon

src/game/client/gameclient.cpp
@@ -3233,8 +3233,8 @@ IGraphics::CTextureHandle *CGameClient::GetInfclassTexturePtrForDamageType(DAMAG
 		return &m_InfclassSkin.m_SpriteSoldierBomb;
 	case DAMAGE_TYPE::SCIENTIST_MINE:
 		return &m_InfclassSkin.m_SpriteScientistMine;
-//	case DAMAGE_TYPE::BIOLOGIST_MINE:
-//		return &m_InfclassSkin.m_SpriteBiologistMine;
+	case DAMAGE_TYPE::BIOLOGIST_MINE:
+		return &m_InfclassSkin.m_SpriteBiologistMine;
 	case DAMAGE_TYPE::MERCENARY_BOMB:
 		return &m_InfclassSkin.m_SpriteMercenaryBomb;
 //	case DAMAGE_TYPE::WHITE_HOLE:

SollyBunny/ddnet

src/game/client/gameclient.cpp
@@ -4461,8 +4461,8 @@ IGraphics::CTextureHandle *CGameClient::GetInfclassTexturePtrForDamageType(EDama
 		return &m_InfclassSkin.m_SpriteSoldierBomb;
 	case EDamageType::SCIENTIST_MINE:
 		return &m_InfclassSkin.m_SpriteScientistMine;
-//	case EDamageType::BIOLOGIST_MINE:
-//		return &m_InfclassSkin.m_SpriteBiologistMine;
+	case EDamageType::BIOLOGIST_MINE:
+		return &m_InfclassSkin.m_SpriteBiologistMine;
 	case EDamageType::MERCENARY_BOMB:
 		return &m_InfclassSkin.m_SpriteMercenaryBomb;
 //	case EDamageType::WHITE_HOLE: