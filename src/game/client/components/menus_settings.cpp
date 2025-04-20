infclass/infclass-client

Use DDRace HUD for Infclass (if available)

src/game/client/components/menus_settings.cpp
@@ -2830,6 +2830,7 @@ void CMenus::RenderSettingsAppearance(CUIRect MainView)
 		RightView.HSplitTop(SectionTotalMargin + 4 * LineSize, &Section, &RightView);
 		Section.Margin(SectionMargin, &Section);
 
+		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfUseDDRaceHUD, Localize("Use DDRace HUD for Infclass"), &g_Config.m_InfUseDDRaceHUD, &Section, LineSize);
 		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfcShowHookProtection, Localize("Show player hook protection"), &g_Config.m_InfcShowHookProtection, &Section, LineSize);
 	}
 	else if(s_CurTab == APPEARANCE_TAB_CHAT)

SollyBunny/ddnet

src/game/client/components/menus_settings.cpp
@@ -2688,6 +2688,7 @@ void CMenus::RenderSettingsAppearance(CUIRect MainView)
 		RightView.HSplitTop(MarginSmall, nullptr, &LeftView);
 
 		// Switches of various DDRace HUD elements
+		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfUseDDRaceHUD, Localize("Use DDRace HUD for Infclass"), &g_Config.m_InfUseDDRaceHUD, &RightView, LineSize);
 		DoButton_CheckBoxAutoVMarginAndSet(&g_Config.m_InfcShowHookProtection, Localize("Show player hook protection"), &g_Config.m_InfcShowHookProtection, &RightView, LineSize);
 	}
 	else if(s_CurTab == APPEARANCE_TAB_CHAT)