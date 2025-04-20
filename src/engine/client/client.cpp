infclass/infclass-client

Client: Send infclass client version info

src/engine/client/client.cpp
@@ -27,6 +27,8 @@
 #include <engine/storage.h>
 #include <engine/textrender.h>
 
+#include <engine/client/infclass.h>
+#include <engine/client/notifications.h>
 #include <engine/shared/assertion_logger.h>
 #include <engine/shared/compression.h>
 #include <engine/shared/config.h>
@@ -246,6 +248,10 @@ void CClient::SendInfo(int Conn)
 	MsgVer.AddInt(GameClient()->DDNetVersion());
 	MsgVer.AddString(GameClient()->DDNetVersionStr());
 	SendMsg(Conn, &MsgVer, MSGFLAG_VITAL);
+
+	CMsgPacker MsgVerInfclass(NETMSG_CLIENTVER_INFCLASS, true);
+	MsgVerInfclass.AddInt(INFCLASS_CLIENT_VERSION);
+	SendMsg(Conn, &MsgVerInfclass, MSGFLAG_VITAL);
 
 	CMsgPacker Msg(NETMSG_INFO, true);
 	Msg.AddString(GameClient()->NetVersion());

src/game/client/gameclient.cpp
@@ -19,6 +19,8 @@
 #include <engine/textrender.h>
 #include <engine/updater.h>
 
+#include <engine/client/infclass.h>
+
 #include <game/generated/client_data.h>
 #include <game/generated/client_data7.h>
 #include <game/generated/protocol.h>
@@ -1793,6 +1795,10 @@ void CGameClient::OnNewSnapshot()
 		CMsgPacker Msg(NETMSGTYPE_CL_ISDDNETLEGACY, false);
 		Msg.AddInt(DDNetVersion());
 		Client()->SendMsg(i, &Msg, MSGFLAG_VITAL);
+
+		CMsgPacker MsgVerInfclass(NETMSG_CLIENTVER_INFCLASS, true);
+		MsgVerInfclass.AddInt(INFCLASS_CLIENT_VERSION);
+		Client()->SendMsg(i, &MsgVerInfclass, MSGFLAG_VITAL);
 		m_aDDRaceMsgSent[i] = true;
 	}
 

SollyBunny/ddnet

src/engine/client/client.cpp
@@ -26,6 +26,7 @@
 #include <engine/storage.h>
 #include <engine/textrender.h>
 
+#include <engine/client/notifications.h>
 #include <engine/shared/assertion_logger.h>
 #include <engine/shared/compression.h>
 #include <engine/shared/config.h>
@@ -33,6 +34,7 @@
 #include <engine/shared/fifo.h>
 #include <engine/shared/filecollection.h>
 #include <engine/shared/http.h>
+#include <engine/shared/infclass.h>
 #include <engine/shared/masterserver.h>
 #include <engine/shared/network.h>
 #include <engine/shared/packer.h>
@@ -226,6 +228,10 @@ void CClient::SendInfo(int Conn)
 		SendMsg(Conn, &Msg, MSGFLAG_VITAL | MSGFLAG_FLUSH);
 		return;
 	}
+
+	CMsgPacker MsgVerInfclass(NETMSG_CLIENTVER_INFCLASS, true);
+	MsgVerInfclass.AddInt(INFCLASS_CLIENT_VERSION);
+	SendMsg(Conn, &MsgVerInfclass, MSGFLAG_VITAL);
 
 	CMsgPacker Msg(NETMSG_INFO, true);
 	Msg.AddString(GameClient()->NetVersion());

src/game/client/gameclient.cpp
@@ -20,6 +20,8 @@
 #include <engine/textrender.h>
 #include <engine/updater.h>
 
+#include <engine/shared/infclass.h>
+
 #include <game/generated/client_data.h>
 #include <game/generated/client_data7.h>
 #include <game/generated/protocol.h>
@@ -2079,6 +2081,10 @@ void CGameClient::OnNewSnapshot()
 		CMsgPacker Msg(NETMSGTYPE_CL_ISDDNETLEGACY, false);
 		Msg.AddInt(DDNetVersion());
 		Client()->SendMsg(i, &Msg, MSGFLAG_VITAL);
+
+		CMsgPacker MsgVerInfclass(NETMSG_CLIENTVER_INFCLASS, true);
+		MsgVerInfclass.AddInt(INFCLASS_CLIENT_VERSION);
+		Client()->SendMsg(i, &MsgVerInfclass, MSGFLAG_VITAL);
 		m_aDDRaceMsgSent[i] = true;
 	}
 