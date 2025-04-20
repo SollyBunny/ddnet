infclass/infclass-client

Enable turret weapon icon

src/game/client/gameclient.cpp
@@ -3212,10 +3212,10 @@ IGraphics::CTextureHandle *CGameClient::GetInfclassTexturePtrForDamageType(DAMAG
 		return &m_InfclassSkin.m_SpriteMercenaryBomb;
 //	case DAMAGE_TYPE::WHITE_HOLE:
 //		return &m_InfclassSkin.m_SpriteWhiteHole;
-//	case DAMAGE_TYPE::TURRET_DESTRUCTION:
-//		return &m_InfclassSkin.m_SpriteTurretDestruction;
-//	case DAMAGE_TYPE::TURRET_LASER:
-//		return &m_InfclassSkin.m_SpriteTurretLaser;
+	case DAMAGE_TYPE::TURRET_DESTRUCTION:
+		return &m_InfclassSkin.m_SpriteTurretDestruction;
+	case DAMAGE_TYPE::TURRET_LASER:
+		return &m_InfclassSkin.m_SpriteTurretLaser;
 
 	case DAMAGE_TYPE::BOOMER_EXPLOSION:
 		return &m_InfclassSkin.m_SpriteBoomerExplosion;

SollyBunny/ddnet

src/game/client/gameclient.cpp
@@ -4467,10 +4467,10 @@ IGraphics::CTextureHandle *CGameClient::GetInfclassTexturePtrForDamageType(EDama
 		return &m_InfclassSkin.m_SpriteMercenaryBomb;
 //	case EDamageType::WHITE_HOLE:
 //		return &m_InfclassSkin.m_SpriteWhiteHole;
-//	case EDamageType::TURRET_DESTRUCTION:
-//		return &m_InfclassSkin.m_SpriteTurretDestruction;
-//	case EDamageType::TURRET_LASER:
-//		return &m_InfclassSkin.m_SpriteTurretLaser;
+	case EDamageType::TURRET_DESTRUCTION:
+		return &m_InfclassSkin.m_SpriteTurretDestruction;
+	case EDamageType::TURRET_LASER:
+		return &m_InfclassSkin.m_SpriteTurretLaser;
 
 	case EDamageType::BOOMER_EXPLOSION:
 		return &m_InfclassSkin.m_SpriteBoomerExplosion;