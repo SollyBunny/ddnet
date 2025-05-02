47e421db5358f1d6ce4f30922aac0276aca29664
SollyBunny <sollybunn@gmail.com>
2025-05-02 15:48:30 +0000

    Add test for CRaceHelper::TimeFromFinishMessage

--- src/test/race_helper.cpp
+++ src/test/race_helper.cpp
@@ -4,6 +4,7 @@
 
 TEST(RaceHelper, TimeFromStr)
 {
+	// x days(s) x hour(s) x minute(s) x.xxx second(s)
 	EXPECT_EQ(CRaceHelper::TimeFromStr("invalid"), -1);
 	EXPECT_EQ(CRaceHelper::TimeFromStr("1 second(s)"), 1000);
 	EXPECT_EQ(CRaceHelper::TimeFromStr("1.432 second(s)"), 1432);
@@ -13,4 +14,18 @@ TEST(RaceHelper, TimeFromStr)
 	EXPECT_EQ(CRaceHelper::TimeFromStr("6 hour(s) 5 minute(s) 1.432 second(s)"), 6 * 60 * 60 * 1000 + 5 * 60 * 1000 + 1432);
 	EXPECT_EQ(CRaceHelper::TimeFromStr("1 day(s) 6 hour(s) 5 minute(s) 1.432 second(s)"), 1 * 24 * 60 * 60 * 1000 + 6 * 60 * 60 * 1000 + 5 * 60 * 1000 + 1432);
 	EXPECT_EQ(CRaceHelper::TimeFromStr("1 day(s) invalid hour(s) 5 minute(s) 1.432 second(s)"), 1 * 24 * 60 * 60 * 1000 + 5 * 60 * 1000 + 1432);
+}
+
+TEST(RaceHelper, TimeFromFinishMessage)
+{
+	// xxx finished in: x days(s) x hour(s) x minute(s) x.xxx second(s)
+	char aName[256];
+	EXPECT_EQ(CRaceHelper::TimeFromFinishMessage("bun bun finished in: invalid", aName, sizeof(aName)), -1);
+	EXPECT_EQ(aName, "bun bun");
+	EXPECT_EQ(CRaceHelper::TimeFromFinishMessage("roby finished in: 40 second(s)", aName, sizeof(aName)), 40 * 1000);
+	EXPECT_EQ(aName, "roby");
+	EXPECT_EQ(CRaceHelper::TimeFromFinishMessage("finished in finished in: 2 minutes(s) 1 second(s)", aName, sizeof(aName)), 2 * 60 * 1000 + 1000);
+	EXPECT_EQ(aName, "finished in");
+	EXPECT_EQ(CRaceHelper::TimeFromFinishMessage("4.321 second(s) in finished in: 1.234 second(s)", aName, sizeof(aName)), 1234);
+	EXPECT_EQ(aName, "finished in");
 }
