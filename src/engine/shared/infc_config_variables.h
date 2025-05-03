feff113f037c15477d0f738bd35ade14d227584d
SollyBunny <sollybunn@gmail.com>
2025-05-03 20:10:39 +0000

    Fix infc_config_variables.h preamble

--- src/engine/shared/infc_config_variables.h
+++ src/engine/shared/infc_config_variables.h
@@ -1,13 +1,10 @@
-/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
-/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+// This file can be included several times.
 
 #ifndef MACRO_CONFIG_INT
-
-#error "Config macros must be defined"
+#error "The config macros must be defined"
 #define MACRO_CONFIG_INT(Name, ScriptName, Def, Min, Max, Save, Desc) ;
 #define MACRO_CONFIG_COL(Name, ScriptName, Def, Save, Desc) ;
 #define MACRO_CONFIG_STR(Name, ScriptName, Len, Def, Save, Desc) ;
-
 #endif
 
 MACRO_CONFIG_INT(ClPreferIconButtons, cl_prefer_icon_buttons, 1, 1, 1, CFGFLAG_SAVE | CFGFLAG_CLIENT, "Prefer icon buttons over text labels")
