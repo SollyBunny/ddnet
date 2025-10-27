cd4cffc11742295796837ac8638fae72e7a45c40
ChillerDragon <chillerdragon@gmail.com>
2025-10-27 10:59:40 +0100

    debug: crash production to trace a ddrace team bug
    Might help with #387
    
    https://github.com/ddnet-insta/ddnet-insta/issues/387#issuecomment-3448505825
    
    This branch should not be hit. But it is and I would like to know how.

--- src/game/server/gamemodes/base_pvp/base_pvp.cpp
+++ src/game/server/gamemodes/base_pvp/base_pvp.cpp
@@ -1255,7 +1255,7 @@ bool CGameControllerPvp::OnSetDDRaceTeam(int ClientId, int Team)
 	CCharacter *pChr = GameServer()->GetPlayerChar(ClientId);
 	if(pChr && pChr->IsAlive())
 	{
-		// the /team chat command is block for in game players
+		// the /team chat command is blocked for in game players
 		// so only death should move you to team 0
 		// any other cases is unexpected and should be investigated.
 		//
@@ -1263,12 +1263,18 @@ bool CGameControllerPvp::OnSetDDRaceTeam(int ClientId, int Team)
 		// but we might be able to recover using SetTeam so this is not an assert
 		//
 		// UPDATE: I saw this log in production. Can not reproduce yet.
-		log_error(
-			"ddnet-insta",
+
+		// log_error(
+		// 	"ddnet-insta",
+		// 	"cid=%d changed from ddrace team %d to ddrace team 0 but is still alive",
+		// 	ClientId,
+		// 	OldDDRaceTeam);
+		// pPlayer->SetTeam(TEAM_SPECTATORS);
+
+		dbg_assert(false,
 			"cid=%d changed from ddrace team %d to ddrace team 0 but is still alive",
 			ClientId,
 			OldDDRaceTeam);
-		pPlayer->SetTeam(TEAM_SPECTATORS);
 	}
 	else
 	{
