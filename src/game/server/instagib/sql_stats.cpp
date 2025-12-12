d12962050c328373179e0f853288bbf7d1374680
ChillerDragon <chillerdragon@gmail.com>
2025-12-12 16:25:33 +0100

    db: move ddnet database helpers to git submodule

--- src/game/server/instagib/sql_stats.cpp
+++ src/game/server/instagib/sql_stats.cpp
@@ -46,12 +46,6 @@ void CInstaSqlResult::SetVariant(EInstaSqlRequestType RequestType)
 	}
 }
 
-// hack to avoid editing connection.h in ddnet code
-ESqlBackend CSqlStats::DetectBackend(IDbConnection *pSqlServer)
-{
-	return str_comp(pSqlServer->BinaryCollate(), "BINARY") == 0 ? ESqlBackend::SQLITE3 : ESqlBackend::MYSQL;
-}
-
 std::shared_ptr<CInstaSqlResult> CSqlStats::NewInstaSqlResult(int ClientId)
 {
 	CPlayer *pCurPlayer = GameServer()->m_apPlayers[ClientId];
@@ -1071,6 +1065,7 @@ bool CSqlStats::CreateTableThread(IDbConnection *pSqlServer, const ISqlData *pGa
 
 	bool (*pfnAddInt)(IDbConnection *, const char *, const char *, char *, int) = nullptr;
 
+	using namespace ddnet_db_utils;
 	if(DetectBackend(pSqlServer) == ESqlBackend::SQLITE3)
 		pfnAddInt = AddColumnIntDefault0Sqlite3;
 	else if(DetectBackend(pSqlServer) == ESqlBackend::MYSQL)
@@ -1089,88 +1084,6 @@ bool CSqlStats::CreateTableThread(IDbConnection *pSqlServer, const ISqlData *pGa
 	}
 
 	return true;
-}
-
-bool CSqlStats::AddIntColumn(IDbConnection *pSqlServer, const char *pTableName, const char *pColumnName, int Default, char *pError, int ErrorSize)
-{
-	char aBuf[4096];
-	str_format(
-		aBuf,
-		sizeof(aBuf),
-		"ALTER TABLE %s ADD COLUMN %s INTEGER DEFAULT %d;", pTableName, pColumnName, Default);
-
-	if(!pSqlServer->PrepareStatement(aBuf, pError, ErrorSize))
-	{
-		return false;
-	}
-	pSqlServer->Print();
-	int NumInserted;
-	return pSqlServer->ExecuteUpdate(&NumInserted, pError, ErrorSize);
-}
-
-bool CSqlStats::AddColumnIntDefault0Sqlite3(IDbConnection *pSqlServer, const char *pTableName, const char *pColumnName, char *pError, int ErrorSize)
-{
-	char aBuf[4096];
-	str_copy(
-		aBuf,
-		"SELECT COUNT() FROM pragma_table_info(?) WHERE name = ?;");
-	if(!pSqlServer->PrepareStatement(aBuf, pError, ErrorSize))
-	{
-		dbg_msg("sql-thread", "prepare failed query: %s", aBuf);
-		return false;
-	}
-	pSqlServer->BindString(1, pTableName);
-	pSqlServer->BindString(2, pColumnName);
-	pSqlServer->Print();
-
-	bool End;
-	if(!pSqlServer->Step(&End, pError, ErrorSize))
-	{
-		dbg_msg("sql-thread", "step failed query: %s", aBuf);
-		return false;
-	}
-
-	if(End)
-	{
-		// we expect 0 or 1 but never nothing
-		dbg_msg("sql-thread", "something went wrong failed query: %s", aBuf);
-		return false;
-	}
-	if(pSqlServer->GetInt(1) == 0)
-	{
-		log_info("sql-thread", "adding missing sqlite3 column '%s' to '%s'", pColumnName, pTableName);
-		return !AddIntColumn(pSqlServer, pTableName, pColumnName, 0, pError, ErrorSize);
-	}
-	return true;
-}
-
-bool CSqlStats::AddColumnIntDefault0Mysql(IDbConnection *pSqlServer, const char *pTableName, const char *pColumnName, char *pError, int ErrorSize)
-{
-	char aBuf[4096];
-	str_format(
-		aBuf,
-		sizeof(aBuf),
-		"show columns from %s where Field = ?;",
-		pTableName);
-	if(!pSqlServer->PrepareStatement(aBuf, pError, ErrorSize))
-	{
-		dbg_msg("sql-thread", "prepare failed query: %s", aBuf);
-		return false;
-	}
-	pSqlServer->BindString(1, pColumnName);
-	pSqlServer->Print();
-
-	bool End;
-	if(!pSqlServer->Step(&End, pError, ErrorSize))
-	{
-		dbg_msg("sql-thread", "step failed query: %s", aBuf);
-		return false;
-	}
-	if(!End)
-		return true;
-
-	log_info("sql-thread", "adding missing mysql column '%s' to '%s'", pColumnName, pTableName);
-	return !AddIntColumn(pSqlServer, pTableName, pColumnName, 0, pError, ErrorSize);
 }
 
 bool CSqlStats::CreateFastcapTableThread(IDbConnection *pSqlServer, const ISqlData *pGameData, Write w, char *pError, int ErrorSize)
