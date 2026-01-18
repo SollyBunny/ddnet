0787b6d0ec70028363560ecbf770a92a9ba037ae
ChillerDragon <ChillerDragon@gmail.com>
2026-01-18 15:28:16 +0100

    refactor: replace hack with workaround

--- src/game/server/gamemodes/vanilla/fly/fly.h
+++ src/game/server/gamemodes/vanilla/fly/fly.h
@@ -11,5 +11,11 @@ public:
 
 	void Tick() override;
 	int OnCharacterDeath(class CCharacter *pVictim, class CPlayer *pKiller, int Weapon) override;
+
+	// TODO: this breaks the config sv_suicide_penalty
+	//       should be off by default in this mode instead of always off
+	//       see https://github.com/ddnet-insta/ddnet-insta/pull/433
+	//       and https://github.com/ddnet-insta/ddnet-insta/issues/308
+	bool HasSuicidePenalty(CPlayer *pPlayer) const override { return false; }
 };
 #endif
