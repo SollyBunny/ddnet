99b1a183f10b0bd1755d5deae1104efcdd979160
ChillerDragon <chillerdragon@gmail.com>
2026-01-19 11:50:32 +0100

    refactor: move csv player class definition and rename it
    I would like to use the round_stats_player.h file for actual round
    stats. And whatever was in there is only used for temporary csv
    sorting. So I moved it to that file and made the name clearer.
    If other stats formats need it too we can move it again.

--- src/game/server/instagib/round_stats_player.h
+++ src/game/server/instagib/round_stats_player.h
@@ -1,20 +1,4 @@
 #ifndef GAME_SERVER_INSTAGIB_ROUND_STATS_PLAYER_H
 #define GAME_SERVER_INSTAGIB_ROUND_STATS_PLAYER_H
 
-class CStatsPlayer
-{
-public:
-	const char *m_pName;
-	int m_Score;
-	int m_Active;
-	bool m_IsDead;
-	CStatsPlayer()
-	{
-		m_pName = "";
-		m_Score = 0;
-		m_Active = false;
-		m_IsDead = false;
-	}
-};
-
 #endif
